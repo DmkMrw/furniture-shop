efb74d834e704fc09e9309642b9777af7a8c979c
Dominik Mrówka <dmrowka7@gmail.com>
2022-07-23 13:14:20 +0200

    Remove unnecessary code

--- src/components/features/BrandsBox/BrandsBox.js
+++ src/components/features/BrandsBox/BrandsBox.js
@@ -1,55 +1,32 @@
 import React from 'react';
 import styles from './BrandsBox.module.scss';
 
-import Carousel from '../../common/Carousel/Carousel';
-import { useSelector } from 'react-redux';
-import clsx from 'clsx';
-import { getAllBrandsPics } from '../../../redux/brandsRedux';
-import { useState } from 'react';
-
 const BrandsBox = () => {
-  const brands = useSelector(state => getAllBrandsPics(state));
-  console.log('brands', brands);
-  const [fade, setFade] = useState(false);
-
-  const handleFade = () => {
-    setFade(true);
-    setTimeout(() => {
-      setFade(false);
-    }, 500);
-  };
-
   return (
     <div className='container'>
       <hr />
       <div className={styles.brandsWrapper}>
-        {/* <div className={styles.button}>
+        <div className={styles.button}>
           <span className={styles.arrow}>&lt;</span>
-        </div> */}
-        <div className={clsx('col-12 px-0', fade && styles.fade)}>
-          <Carousel products={brands} handleParentFade={handleFade} parentFade={fade} />
         </div>
-        {/* <div className={styles.imgSize}>
-
-        {
-          <img src={`${process.env.PUBLIC_URL + brands[0].picture}`} />
-
-
+        <div className={styles.imgSize}>
+          <img src={`${process.env.PUBLIC_URL}/images/brand1.png`} />
+          <img src={`${process.env.PUBLIC_URL}/images/brand2.png`} />
           <img src={`${process.env.PUBLIC_URL}/images/brand3.png`} />
-          <img src={`${process.env.PUBLIC_URL}/images/brand2.webp`} />
+          <img src={`${process.env.PUBLIC_URL}/images/brand2.png`} />
           <img src={`${process.env.PUBLIC_URL}/images/brand3.png`} />
-          <img src={`${process.env.PUBLIC_URL}/images/brand1.webp`} />
-          <img src={`${process.env.PUBLIC_URL}/images/brand2.webp`} />
+          <img src={`${process.env.PUBLIC_URL}/images/brand1.png`} />
+          <img src={`${process.env.PUBLIC_URL}/images/brand2.png`} />
           <img src={`${process.env.PUBLIC_URL}/images/brand3.png`} />
-          <img src={`${process.env.PUBLIC_URL}/images/brand2.webp`} />
+          <img src={`${process.env.PUBLIC_URL}/images/brand2.png`} />
           <img src={`${process.env.PUBLIC_URL}/images/brand3.png`} />
-          <img src={`${process.env.PUBLIC_URL}/images/brand2.webp`} />
+          <img src={`${process.env.PUBLIC_URL}/images/brand2.png`} />
           <img src={`${process.env.PUBLIC_URL}/images/brand3.png`} />
-          <img src={`${process.env.PUBLIC_URL}/images/brand1.webp`} />
+          <img src={`${process.env.PUBLIC_URL}/images/brand1.png`} />
         </div>
         <div className={styles.button}>
           <span className={styles.arrow}>&gt;</span>
-        </div> */}
+        </div>
       </div>
       <hr />
     </div>
