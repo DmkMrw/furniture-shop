cc905e60eb4cc4b2a39930282b1130ef03abf108
Dominik Mrówka <dmrowka7@gmail.com>
2022-07-24 20:02:12 +0200

    Change Popup display

--- src/components/features/ProductPopup/ProductPopup.js
+++ src/components/features/ProductPopup/ProductPopup.js
@@ -3,33 +3,41 @@ import PropTypes from 'prop-types';
 import styles from './ProductPopup.module.scss';
 import StarRating from '../StarRating/StarRating';
 
-const ProductPopup = ({ image, name, id, ownStars, stars, price, handleClose }) => {
+const ProductPopup = ({
+  image,
+  name,
+  id,
+  ownStars,
+  stars,
+  price,
+  oldPrice,
+  handleClose,
+}) => {
   return (
-    <div className={styles.popup_Box}>
-      <div className={styles.box}>
-        <div className='row'>
-          <div className='col-6'>
-            <div className={styles.productImage}>
-              <img
-                className={styles.image}
-                src={`${process.env.PUBLIC_URL}` + image}
-                alt='ProductImage'
-              />
-            </div>
-          </div>
-          <div className='col-6'>
-            <div className={styles.content}>
-              <h5>{name}</h5>
-              <div className={styles.stars}>
-                <StarRating id={id} ownStars={ownStars} stars={stars} />
+    <div className={styles.root}>
+      <div className={styles.popup_Box}>
+        <div className={styles.box}>
+          <div className='container row m-auto justify-content-center'>
+            <div className='col-12 col-md-9 col-lg-6 px-0 pr-md-3'>
+              <div>
+                <div className={styles.galleryContent}>
+                  <img src={`${process.env.PUBLIC_URL}` + image} alt='chair' />
+                  <span className={styles.close_icon} onClick={handleClose}>
+                    x
+                  </span>
+                  <div className={styles.productData}>
+                    <div className={styles.promotionInfo}>
+                      <p>{price}.00$</p>
+                      {oldPrice && <p>{oldPrice}.00$</p>}
+                    </div>
+                    <h5>{name}</h5>
+                    <StarRating id={id} stars={stars} ownStars={ownStars} />
+                  </div>
+                </div>
               </div>
             </div>
-            <div className={styles.price}>$ {Number.parseFloat(price).toFixed(2)}</div>
           </div>
         </div>
-        <span className={styles.close_icon} onClick={handleClose}>
-          x
-        </span>
       </div>
     </div>
   );
