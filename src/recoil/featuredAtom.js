edf29aef5e14cf4379913228405a7553357ce29e
Milosz B <miloszbargiel@gmail.com>
2022-07-24 14:01:45 +0200

    Add featured atom

---
+++ src/recoil/featuredAtom.js
@@ -0,0 +1,6 @@
+import { atom } from 'recoil';
+
+export const featuredState = atom({
+  key: 'featured',
+  default: ['aenean-ru-bristique-9', 'aenean-ru-bristique-8', 'aenean-ru-bristique-13'],
+});
