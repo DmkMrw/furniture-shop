6bbfd9fea85296a280b3453add1e81ac38539e39
Milosz B <miloszbargiel@gmail.com>
2022-07-24 15:53:48 +0200

    Add new constants and modify the naming convention of breakpoints

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,10 @@
 //animation times
 export const fadeDurationInMs = 1000;
 export const contentRefreshDelayInMs = 500;
-//screen width for carousel breakpoints
-export const mobileBreakpoint = 400;
+//screen width breakpoints
+export const smallMobileBreakpoint = 400;
+export const mobileBreakpoint = 768;
+export const tabletBreakpoint = 992;
 export const desktopBreakpoint = 1200;
 //number of items per page in the carousel
 export const mobileItemsPerPage = 3;
@@ -10,3 +12,8 @@ export const tabletItemsPerPage = 5;
 export const desktopItemsPerPage = 6;
 //product comparator variables
 export const comparedProductsLimit = 4;
+//new furniture variables
+export const productsPerPageDesktop = 8;
+export const productsPerPageLaptop = 6;
+export const productsPerPageTablet = 4;
+export const productsPerPageMobile = 2;
